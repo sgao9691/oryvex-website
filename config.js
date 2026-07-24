83abb635d2e3aef871b89dae01261e47190afdce
Shengyun Gao <sgao9691@icloud.com>
2026-07-23 17:13:27 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // ===== REPLACE ALL PLACEHOLDERS HERE =====
 window.SITE_CONFIG = {
-  companyName: "Oryvex",
+  companyName: "ORYVEX",
   legalName: "Oryvex Analytics LLC",
   email: "contact@oryvexdata.com",
   location: "Los Angeles, California"
